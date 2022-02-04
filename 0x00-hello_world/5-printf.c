c53539600185314d9aa764c082376439c5c970a9
Your Name <you@example.com>
2022-02-03 17:24:35 -0800

    file created

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "with proper grammar, but the outcome is a piece of art, "
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art, "
+	);
+	return (0);
+}
