51cf5cd182c7cf257f40626f0ca7f0f7a23d99a7
Your Name <you@example.com>
2022-02-04 16:39:27 -0800

    fixed: syntax of the code

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: "The variable n will store a different value"
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+	{
+		int n;
+		int last_digit;
+
+		srand(time(0));
+
+		n = rand() - RAND_MAX / 2;
+		last_digit = n % 10;
+		if (n > 5)
+		{
+			printf("Last digit of %d is %d and is greater than 5", n, last_digit);
+			printf("\n");
+		}
+		if (n == 0)
+		{
+			printf("Last digit of %d is %d and is 0", n, last_digit);
+			printf("\n");
+		}
+		if (n < 6 && n != 0)
+		{
+			printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
+			printf("\n");
+		}
+		return (0);
+	}
