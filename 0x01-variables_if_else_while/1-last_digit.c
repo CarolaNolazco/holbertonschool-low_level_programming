e370faddceb06ea8a4f2f8207f41e0f9e3d16318
Your Name <you@example.com>
2022-02-04 17:37:38 -0800

    fixed: code fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0", n, l);
 		printf("\n");
 	}
-	if (l < 6 && n != 0)
+	if (l < 6 && l != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, l);
 		printf("\n");
