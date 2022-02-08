df8d3070e827bd6e66cabe8d0e137e21239125f9
Your Name <you@example.com>
2022-02-08 15:25:00 -0800

    code works as it is expected

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase"
+ *
+ * Return: Always (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 97 ; j < 123 ; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
