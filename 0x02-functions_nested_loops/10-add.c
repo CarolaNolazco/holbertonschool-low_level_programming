e00ace3b752fae481c8c5661c60c6e9b67f7c8a5
Your Name <you@example.com>
2022-02-08 21:43:13 -0800

    code workd as it is expected

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * add - "adds two integers and returns the result"
+ *
+ * @i: character to prints
+ *
+ * @j: character to prints
+ *
+ * Return: Always (0)
+ */
+
+int add(int i, int j)
+{
+	int suma;
+
+	suma = i + j;
+
+	return (suma);
+}
+
+
+
+
