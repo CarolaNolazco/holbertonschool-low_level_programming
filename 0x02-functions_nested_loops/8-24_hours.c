4e7c6951e8a33b73307ca9ebfdd7a96d1decf7bc
Your Name <you@example.com>
2022-02-08 20:10:47 -0800

    code works as it is expected

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - "prints the sign of a number"
+ *
+ * Return: Always (0)
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48 ; i < 51 ; i++)
+	{
+		for (j = 48 ; j < 52 ; j++)
+		{
+			for (k = 48 ; k < 54 ; k++)
+			{
+				for (l = 48 ; l < 58 ; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
