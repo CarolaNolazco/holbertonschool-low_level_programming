2414b43d9e43bd19e58ec170860d8af7449d3fa5
Your Name <you@example.com>
2022-02-08 21:37:47 -0800

    code fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,24 +2,24 @@
 
 /**
  * jack_bauer - "prints every minute of the day from 00:00 to 23:59"
- * 
+ *
  * Return: Always (0)
  */
 
 void jack_bauer(void)
 {
 	int hours;
-	int minutes;
+	int min;
 
 	for (hours = 0 ; hours < 24 ; hours++)
 	{
-		for (minutes = 0 ; minutes < 60 ; minutes++)
+		for (min = 0 ; min  < 60 ; min++)
 		{
 			_putchar((hours / 10) + '0');
 			_putchar((hours % 10) + '0');
 			_putchar(':');
-			_putchar((minutes / 10) + '0');
-			_putchar((minutes % 10) + '0');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar(' ');
 			_putchar('\n');
 		}
