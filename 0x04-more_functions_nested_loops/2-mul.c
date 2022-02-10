bf26f5134667ada7c62e4139163c33d30a87b2c4
Your Name <you@example.com>
2022-02-10 09:29:46 -0800

    code multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - "checks for lowercase character"
+ *
+ * @a: character to check
+ *
+ * @b: character to check
+ *
+ * Return: Always (0)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
