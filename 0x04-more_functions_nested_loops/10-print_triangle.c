3f07ec5aa15501e4c3558e844035397a4064f580
Your Name <you@example.com>
2022-02-10 20:27:28 -0800

    code pritns a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,6 +12,7 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
 	if (size <= 0)
 	{
@@ -21,11 +22,11 @@ void print_triangle(int size)
 
 	for (i = 0 ; i < size ; i++)
 	{
-		for (j = 1 ; j <= size - i ; j++)
+		for (j = 2 ; j <= size - i ; j++)
 		{
-			_putchar(32);
+			_putchar(' ');
 		}
-		for (j = 0; j <= i ; j++)
+		for (k = 0 ; k <= i ; k++)
 		{
 			_putchar('#');
 		}
