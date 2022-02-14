fc813e488e08eaf7be542eec467cb7b18ca83596
Your Name <you@example.com>
2022-02-14 14:32:27 -0800

    function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - "checks for lowercase character"
+ *
+ * @s: character to check
+ *
+ * Return: Always (0)
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
