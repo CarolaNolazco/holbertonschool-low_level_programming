a21245ef053eb5f2895481b28189a6f227b0ee8f
Your Name <you@example.com>
2022-02-14 15:30:39 -0800

    function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - "prints a string"
+ *
+ * @str: character to check
+ *
+ * Return: Always (0)
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		i++;
+		str++;
+	}
+	_putchar(10);
+}
