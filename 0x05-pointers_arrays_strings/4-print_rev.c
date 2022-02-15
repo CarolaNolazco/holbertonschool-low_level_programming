5631898b1f9c06fa8b0bdc6367b22f48a22e7412
Your Name <you@example.com>
2022-02-14 20:00:07 -0800

    feat: fixed the extra character

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_rev - "checks for lowercase character"
+ *
+ * @s: character to check
+ *
+ * Return: Always (0)
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+
+	s--;
+
+	while (i > 0)
+	{
+		_putchar(*s);
+		s = s - 1;
+		i--;
+	}
+	_putchar(10);
+}
