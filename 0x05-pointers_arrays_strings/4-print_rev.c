667544c5c1ba8f619ff5e03c580c32577368544d
Your Name <you@example.com>
2022-02-14 18:43:09 -0800

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - "checks for lowercase character"
+ *
+ * @s: character to check
+ *
+ * Return: Always (0)
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int counter = i;
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		i++;
+		s++;
+	}
+	while(i >= 0)
+	{
+		_putchar(*s);
+		counter--;
+	}
+	_putchar(10);
+}
